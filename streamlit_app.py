6a413de1fdf17fc2df4be93b046fce34bfa3617f
Ashwath <ashwath.1si13te012@gmail.com>
2025-09-26 05:25:36 +0000

    small edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My Portfolio")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Hello, world! Let's start building! To explore my work, head over to [https://ashwathhalemane.github.io/](https://https://ashwathhalemane.github.io)."
 )
